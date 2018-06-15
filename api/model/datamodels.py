35b67f7ecaec27227d64a3bc7a313d38641b6455
Prakhar Asthana <prakhar.asthana04@gmail.com>
2018-06-15 22:36:57 +0530

    Updated attributes as per the external API

--- api/model/datamodels.py
+++ api/model/datamodels.py
@@ -1,31 +1,22 @@
 class Match():
-    def __init__(self, home_team, away_team, home_team_goal, away_team_goal, date, **kwargs):
-        self.home_team = home_team
-        self.away_team = away_team
-        self.home_team_goal = home_team_goal
-        self.away_team_goal = away_team_goal
-        self.date = date
+    def __init__(self, home_team, away_team, datetime, winner, **kwargs):
+        self.home_team = home_team.country
+        self.away_team = away_team.country
+        self.datetime = datetime
+        self.winner = winner
 
 
 class PastMatch(Match):
 
-    def __init__(self, home_team, away_team, home_team_goal, away_team_goal, date, **kwargs):
-        super().__init__(home_team, away_team, home_team_goal, away_team_goal, date, **kwargs)
-        self.winner = self._get_winner(home_team, away_team, home_team_goal, away_team_goal)
-
-    def _get_winner(self, home_team, away_team, home_team_goal, away_team_goal):
-        if home_team_goal > away_team_goal:
-            return home_team
-        elif away_team_goal < home_team_goal:
-            return away_team
-        else:
-            return "Tie"
+    def __init__(self, home_team, away_team, datetime, winner, **kwargs):
+        super().__init__(home_team, away_team, datetime, **kwargs)
+        self.winner = winner
 
 
 class CurrentMatch(Match):
-    def __init__(self, home_team, away_team, home_team_goal, away_team_goal, date, time, home_team_event,
-                 away_team_event, **kwargs):
-        super().__init__(home_team, away_team, home_team_goal, away_team_goal, date, **kwargs)
+    def __init__(self, home_team, away_team, home_team_goal, away_team_goal, datetime, time, home_team_events,
+                 away_team_events, **kwargs):
+        super().__init__(home_team, away_team, home_team_goal, away_team_goal, datetime, time, **kwargs)
         self.time = time
-        self.home_team_event = home_team_event
-        self.away_team_event = away_team_event
+        self.home_team_event = home_team_events[-1]
+        self.away_team_event = away_team_events[-1]
