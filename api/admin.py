efc3e46e86040f05e6e25f28d134efa7a8698812
Prakhar Asthana <prakhar.asthana04@gmail.com>
2018-06-15 19:19:30 +0530

    Added model for admin portal

--- api/admin.py
+++ api/admin.py
@@ -3,4 +3,6 @@ from . import models
 
 
 # Register your models here.
-admin.site.register(models.Match)
+admin.site.register(models.PastMatchModel)
+admin.site.register(models.CurrentMatchModel)
+admin.site.register(models.FutureMatchModel)
