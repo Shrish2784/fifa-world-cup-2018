8bf363f9e6ae915c6e5768b4622b467fd42a718e
Prakhar Asthana <prakhar.asthana04@gmail.com>
2018-06-15 22:39:02 +0530

    Updated attributes as per the external API

--- api/model/datamodels.py
+++ api/model/datamodels.py
@@ -1,7 +1,7 @@
 class Match():
     def __init__(self, home_team, away_team, datetime, winner, **kwargs):
-        self.home_team = home_team.country
-        self.away_team = away_team.country
+        self.home_team = home_team
+        self.away_team = away_team
         self.datetime = datetime
         self.winner = winner
 
@@ -9,14 +9,13 @@ class Match():
 class PastMatch(Match):
 
     def __init__(self, home_team, away_team, datetime, winner, **kwargs):
-        super().__init__(home_team, away_team, datetime, **kwargs)
-        self.winner = winner
+        super().__init__(home_team, away_team, datetime, winner, **kwargs)
 
 
 class CurrentMatch(Match):
-    def __init__(self, home_team, away_team, home_team_goal, away_team_goal, datetime, time, home_team_events,
+    def __init__(self, home_team, away_team, datetime, time, winner, home_team_events,
                  away_team_events, **kwargs):
-        super().__init__(home_team, away_team, home_team_goal, away_team_goal, datetime, time, **kwargs)
+        super().__init__(home_team, away_team, datetime, winner, **kwargs)
         self.time = time
         self.home_team_event = home_team_events[-1]
         self.away_team_event = away_team_events[-1]
