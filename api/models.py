73638010b1f2c856320e301a79333b83a171d775
shrish2784 <shrishasthana@outlook.comgit config --global user.email shrishasthana@outlook.com>
2018-06-15 13:28:16 +0530

    Create Database

--- api/models.py
+++ api/models.py
@@ -2,9 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Match(models.Model):
-    # away_team = models.CharField(max_length=250)
-    # home_team = models.CharField(max_length=250)
-    # goal_home = models.IntegerField(blank=True)
-    # goal_away = models.IntegerField(blank=True)
-
     matches = models.CharField(max_length=20000, null=True)
