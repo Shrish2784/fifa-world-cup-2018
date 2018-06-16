a74c71dfdb1b234b37af7e32c5500b1fb0959126
Prakhar Asthana <prakhar.asthana04@gmail.com>
2018-06-16 15:14:21 +0530

    Updated Api URL path

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('match/', views.index, name='index'),
+    path('matches/', views.index, name='index'),
     path('sync/currentMatch/', views.sync_current_match, name='sync_currentMatch'),
     path('sync/matches/', views.sync_past_future_match, name='sync_matches')
 ]
