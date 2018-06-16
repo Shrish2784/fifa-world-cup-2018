b14ff31307c4a86b94f46bf4960e3c4e79978624
Prakhar Asthana <prakhar.asthana04@gmail.com>
2018-06-16 13:06:55 +0530

    Updated URL path to get match info

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('sync/currentMatch', views.sync_current_match, name='sync_currentMatch'),
-    path('sync/matches', views.sync_past_future_match, name='sync_matches')
+    path('match/', views.index, name='index'),
+    path('sync/currentMatch/', views.sync_current_match, name='sync_currentMatch'),
+    path('sync/matches/', views.sync_past_future_match, name='sync_matches')
 ]
