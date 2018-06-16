6f38b138970892ac6f04796d32641f78c2ca9da9
Prakhar Asthana <prakhar.asthana04@gmail.com>
2018-06-16 13:03:32 +0530

    Fixed Bug for future match details

--- api/views.py
+++ api/views.py
@@ -8,14 +8,21 @@ import requests
 url = 'http://worldcup.sfg.io/matches/{}'
 
 
+def validate_data(res):
+    if len(res) > 0:
+        match_details = res.order_by("-id")[0].match_details
+        match_json = json.loads(match_details)
+        if len(match_json) > 0:
+            return {'status': True, 'match_json': match_json}
+    return {'status': False}
+
+
 def index(request):
     response = models.CurrentMatchModel.objects.all()
-    if len(response) > 0:
-        match_json = json.loads(response.order_by("-id")[0].match_details)
-        print(match_json)
-        match_object = datamodels.CurrentMatch(**match_json[0])
+    result = validate_data(response)
+    if result['status']:
+        match_object = datamodels.CurrentMatch(**result['match_json'][0])
         return HttpResponse(json.dumps(match_object.__dict__))
-
     else:
         d = str(datetime.datetime.now())
         time = int(d[11: 13])
@@ -46,20 +53,23 @@ def sync_current_match(request):
 def sync_past_future_match(request):
     completed = []
     future = []
+    n_completed_matches = 0
     matches = requests.get(url.format('')).json()
     print("-----------------Scheduled job sync_matches ran-----------------------")
     for match in matches:
         if match['status'] == 'completed':
             completed.append(match)
+            n_completed_matches += 1
         elif match['status'] == 'future':
-            match_json = json.dumps(match)
+            match_json = json.loads(match)
             future.append(datamodels.Match(**match_json))
             if len(future) >= 3:
                 break
+    if n_completed_matches >= 3:
+        completed = completed[(len(completed) - 3): len(completed)]
 
-    completed = completed[(len(completed) - 3): len(completed)]
     for i in range(len(completed)):
-        match_json = json.dumps(completed[i])
+        match_json = json.loads(completed[i])
         completed[i] = datamodels.PastMatch(**match_json)
 
     model = models.PastMatchModel.objects.create(matches=json.dumps(completed))
