c01b7942096567ccf18d492edc86a7cda7cedb32
Prakhar Asthana <prakhar.asthana04@gmail.com>
2018-06-16 13:22:32 +0530

    Fixed Bug for Clock API

--- api/clock.py
+++ api/clock.py
@@ -7,13 +7,13 @@ url = 'https://worldcup-18.herokuapp.com/{}'
 
 @sched.scheduled_job('interval', minutes=.1)
 def sync_current_match():
-    res = requests.get(url.format('sync/currentMatch')).json()
+    res = requests.get(url.format('sync/currentMatch'))
     print(res)
 
 
 @sched.scheduled_job('interval', minutes=.2)
 def sync_matches():
-    res = requests.get(url.format('sync/matches')).json()
+    res = requests.get(url.format('sync/matches'))
     print(res)
 
 
