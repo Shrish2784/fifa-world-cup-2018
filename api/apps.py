d9d584c1deac52c6f6d3b7ee3df2ac3a823928c7
Abhishek Singh <abhishek.singh@iitgn.ac.in>
2018-06-18 21:13:54 +0530

    Added the one time job.

--- api/apps.py
+++ api/apps.py
@@ -10,8 +10,8 @@ class ApiConfig(AppConfig):
 
         print("-------------------------RAN READY FUNCTION----------------------------")
 
-        # global names
-        # file = open('validation_data/names.txt')
-        # for f in file:
-        #     l = list(f.split("\n"))
-        #     names.add(l[0])
+        global names
+        file = open('validation_data/names.txt')
+        for f in file:
+            l = list(f.split("\n"))
+            names.add(l[0])
