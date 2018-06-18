abe28bdd678df0e20a22d792673e82cc96aeb8d8
Abhishek Singh <abhishek.singh@iitgn.ac.in>
2018-06-18 13:35:29 +0530

    Added urls for the test API.

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('matches/', views.index, name='index'),
     path('sync/current_match/', views.sync_current_match, name='sync_current_match'),
-    path('sync/matches/', views.sync_matches, name='sync_matches')
+    path('sync/matches/', views.sync_matches, name='sync_matches'),
+    path('test/past/', views.past_match_response, name='test_past'),
+    path('test/current/', views.current_match_response, name='test_current'),
+    path('test/future/', views.future_match_response, name='test_future'),
 ]
