148377f6364fc3341bbe3fbcdb2158092cb46400
Abhishek Singh <abhishek.singh@iitgn.ac.in>
2018-06-20 16:50:34 +0530

    Decreased the frequency of syncing matches

--- api/clock.py
+++ api/clock.py
@@ -5,13 +5,13 @@ sched = BlockingScheduler()
 url = 'https://worldcup-18.herokuapp.com/{}'
 
 
-@sched.scheduled_job('interval', minutes=.1)
+@sched.scheduled_job('interval', minutes=.2)
 def sync_current_match():
     res = requests.get(url.format('sync/current_match'))
     print(res)
 
 
-@sched.scheduled_job('interval', minutes=.2)
+@sched.scheduled_job('interval', minutes=6)
 def sync_matches():
     res = requests.get(url.format('sync/matches'))
     print(res)
